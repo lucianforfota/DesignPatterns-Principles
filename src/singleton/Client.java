89d955a802d08b68840a0ae3e267f121cba650a4
LucianForfota <lucianforfota91@gmail.com>
2024-11-24 19:21:23 +0200

    client

--- src/singleton/Client.java
+++ src/singleton/Client.java
@@ -3,8 +3,9 @@ package singleton;
 public class Client {
 
     public static void main(String[] args) {
+        Singleton s0 =Singleton.getInstance();
         Singleton s1 = Singleton.getInstance();
         Singleton s2 = Singleton.getInstance();
-        System.out.println(s1==s2);
+        System.out.println(s0==s1);
     }
 }
